18759df8254477dd362321f5b46bef98e7bcaedd
anuj_chaudhary <anuj.chaudhary@epam.com>
2026-07-09 14:38:44 +0530

    Revert "adds notification lambda code"
    This reverts commit 0adfed741056af6d2a9c544461ac5e22d9aebaa2.

--- src/sqs/sqs.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SqsConsumerService } from './sqs-consumer.service.js';
-import { NotificationModule } from '../notification/notification.module.js';
-
-@Module({
-  imports: [NotificationModule],
-  providers: [SqsConsumerService],
-})
-export class SqsModule {}
